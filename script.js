64404a13d4857828a2061d77a14e031abfa144c4
Михаил Минченко <t9052585903@gmail.com>
2020-10-08 20:11:44 +0300

    finish

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-const getSameParity = arr => {
-	let evenNumber = Math.abs(arr[0]) % 2;
-	let result = [];
+const money1 = [ 'eur 10', 'usd 1', 'usd 10', 'rub 50', 'usd 5' ];
+const money2 = [ 'eur 10', 'usd 1', 'eur 5', 'rub 100', 'eur 20', 'eur 100', 'rub 200' ];
+const money3 = [ 'eur 10', 'rub 50', 'eur 5', 'rub 10', 'rub 10', 'eur 100', 'rub 200' ];
 
-	if (arr.length) {
-		for (const elem of arr) {
-			if (Math.abs(elem) % 2 === evenNumber) {
-				result.push(elem);
-			}
+const getTotalAmount = (arr, currency) => {
+	let result = 0;
+
+	for (const elem of arr) {
+		const elemCurrency = elem.slice(0, 3);
+		const elemValue = elem.slice(4);
+
+		if (elemCurrency !== currency) {
+			continue;
 		}
+		result += +elemValue;
 	}
-
 	return result;
 };
 
-console.log(getSameParity([]));
-console.log(getSameParity([ 1, 2, 3 ]));
-console.log(getSameParity([ 1, 2, 8 ]));
-console.log(getSameParity([ 2, 2, 8 ]));
+console.log(getTotalAmount(money1, 'usd'));
+console.log(getTotalAmount(money2, 'eur'));
+console.log(getTotalAmount(money3, 'rub'));
