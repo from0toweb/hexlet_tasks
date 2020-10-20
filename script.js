42db9775507febb3b75e5c40698a0acbec4bcb11
Михаил Минченко <t9052585903@gmail.com>
2020-10-20 21:27:51 +0300

    complete

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-const getIntersectionOfSortedArrays = (arr1, arr2) => {
-    const result = [];
-    const count = arr1.length + arr2.length;
-    let arr1Ind = 0;
-    let arr2Ind = 0;
+const openingSymbols = ['(', '[', '{', '<'];
+const closingSymbols = [')', ']', '}', '>'];
 
-    for (let i = 0; i < count; i += 1) {
-        if (arr1[arr1Ind] === arr2[arr2Ind]) {
-            result.push(arr1[arr1Ind]);
-            arr1Ind += 1;
-            arr2Ind += 1;
-        }
-        if (arr1[arr1Ind] > arr2[arr2Ind]) {
-            arr2Ind += 1;
-        }
-        if (arr1[arr1Ind] < arr2[arr2Ind]) {
-            arr1Ind += 1;
-        }
 
+export default (expression) => {
+
+    const stack = [];
+
+
+    for (const symbol of expression) {
+        const lastSymbol = stack[stack.length - 1]
+
+
+        if (openingSymbols.indexOf(symbol) !== -1) {
+            stack.push(symbol);
+        } else if (closingSymbols.indexOf(symbol) === openingSymbols.indexOf(lastSymbol))  {
+            stack.pop();
+        } else {
+            return false;
+        }
     }
 
-    return result;
+    return stack.length === 0;
 };
 
 
-console.log(getIntersectionOfSortedArrays([10, 11, 24], [10, 13, 14, 18, 24, 30]));
-console.log(getIntersectionOfSortedArrays([10, 11, 24], [-2, 3, 4]));
-console.log(getIntersectionOfSortedArrays([10, 12, 19, 21], [3, 5, 10]));
+console.log(isBracketStructureBalanced('(>'))
+console.log(isBracketStructureBalanced('()'))
+console.log(isBracketStructureBalanced('[()]'))
+console.log(isBracketStructureBalanced('{<>}}'))
